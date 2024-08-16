58e6ea744829622ff2c0ad35e383fecf8f1ea98e
Antonella-gif <antonellasalaberry2701@gmail.com>
2024-08-15 22:48:55 -0300

    modificacion script

--- js/script.js
+++ js/script.js
@@ -19,20 +19,6 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
-/*fetch(DATA_URL)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Network response was not ok ${response.statusText}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    showData(data);
-  })
-  .catch(error => {
-    console.error('Hubo un problema con la solicitud Fetch:', error);
-  });*/
-
 function getJsonData(DATA_URL){
   let result = {};
   return fetch(DATA_URL) 
